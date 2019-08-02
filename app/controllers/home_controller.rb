5404fb200d71492aebc90179adb4e5358d08455c
raafa16 <raafa16@hotmail.com>
2019-08-02 02:32:02 -0400

    storing airnow API key in credentials.yml. getting json response for current weather observation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+
+    @url =
+      "http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=46239&distance=25&API_KEY=#{ENV["airnow_api_key"] ||  Rails.application.credentials[:airnow][:API_KEY]}"
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
   end
 end
