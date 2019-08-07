0dd9a529e2c13bff3d19756d0c265920776bf7e8
raafa16 <raafa16@hotmail.com>
2019-08-06 22:49:14 -0400

    add current weather and 3hr forecast data. add partials for current weather and forecast. change index to geolocation. page design. overwrite bootstrap css.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,29 +1,23 @@
 class HomeController < ApplicationController
-  def index
+  def geolocation
     require 'net/http'
     require 'json'
 
-    puts "params #{params}"
     if params[:lat_long].present?
       @lat_lang = params[:lat_long].split("|")
-      @url =
+      weather_url =
           "http://api.openweathermap.org/data/2.5/weather?lat=#{@lat_lang[0]}&lon=#{@lat_lang[1]}&appid=#{Rails.application.credentials[:openweather][:API_KEY]}&units=imperial"
-      @uri = URI(@url)
-      @response = Net::HTTP.get(@uri)
-      @output = JSON.parse(@response)
+      @weather_output = JSON.parse(Net::HTTP.get(URI(weather_url)))
+      forecast_url = "http://api.openweathermap.org/data/2.5/forecast?lat=#{@lat_lang[0]}&lon=#{@lat_lang[1]}&cnt=9&appid=#{Rails.application.credentials[:openweather][:API_KEY]}&units=imperial"
+      @forecast_output = JSON.parse(Net::HTTP.get(URI(forecast_url)))
     end
   end
 
   def zipcode
     if params[:zipcode].present?
-      @zipcode = params[:zipcode]
       @url =
-          "http://api.openweathermap.org/data/2.5/weather?zip=#{@zipcode},us&appid=#{Rails.application.credentials[:openweather][:API_KEY]}&units=imperial"
-      @uri = URI(@url)
-      @response = Net::HTTP.get(@uri)
-      @output = JSON.parse(@response)
-    else
+          "http://api.openweathermap.org/data/2.5/weather?zip=#{params[:zipcode]},us&appid=#{Rails.application.credentials[:openweather][:API_KEY]}&units=imperial"
+      @weather_output = JSON.parse(Net::HTTP.get(URI(@url)))
     end
-
   end
 end
